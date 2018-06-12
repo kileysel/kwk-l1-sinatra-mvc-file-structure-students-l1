48fcfcf0ba2a0db7c83e50bdf70016204d28935c
kileysel <kiley@selmanhouse.com>
2018-06-12 23:28:15 +0000

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,9 @@
-class Model
+#class Model
   # Replace with Dog clas
-end
+#end
+def get_fortune()
+  #method goes here 
+  fortunes_array = ["You'll have a great day", "Someones going to the bathroom in the next 20 mins", "You'll have sandwiches for the rest of your life", "Jessica said no", ]
+  return fortunes_array.sample
+end 
+puts get_fortune
